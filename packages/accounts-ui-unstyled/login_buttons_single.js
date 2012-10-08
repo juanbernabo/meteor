bbb85a08f4bad140337aad25fe9660f503238af0
Avital Oliver <avital@thewe.net>
2012-10-08 13:26:50 -0700

    Large refactor of accounts-ui
    - Split login_buttons.html and login_buttons.js into multiple files
      - Specifically, make it easier to reason about the display of loginButtons whether it is in dropdown mode or not
    - Split templates into subtemplates to make it easier to read the login button .html files as "tables of contents"
    - Introduce Meteor._loginButtonsSession, which makes it easier to access internal session fields for loginButtons
    - Unify code that calls the various Meteor.loginWithFoo() functions
    
    Breaking change: rename "configureLoginServicesDialogForFoo" to "configureLoginServiceDialogForFoo" (in packages such as accounts-facebook)

---
+++ packages/accounts-ui-unstyled/login_buttons_single.js
@@ -0,0 +1,36 @@
+(function () {
+  // for convenience
+  var loginButtonsSession = Meteor._loginButtonsSession;
+
+  Template.loginButtonsLoggedOutSingleLoginButton.events({
+    'click .login-button': function () {
+      var serviceName = this.name;
+      Meteor._loginButtonsSession.resetMessages();
+      Meteor["loginWith" + capitalize(serviceName)](function (err) {
+        if (!err) {
+          loginButtonsSession.closeDropdown();
+        } else if (err instanceof Accounts.LoginCancelledError) {
+          // do nothing
+        } else if (err instanceof Accounts.ConfigError) {
+          loginButtonsSession.configureService(serviceName);
+        } else {
+          loginButtonsSession.set('errorMessage', err.reason || "Unknown error");
+        }
+      });
+    }
+  });
+
+  Template.loginButtonsLoggedOutSingleLoginButton.configured = function () {
+    return !!Accounts.configuration.findOne({service: this.name});
+  };
+
+  Template.loginButtonsLoggedOutSingleLoginButton.capitalizedName = function () {
+    return capitalize(this.name);
+  };
+
+  // XXX from http://epeli.github.com/underscore.string/lib/underscore.string.js
+  var capitalize = function(str){
+    str = str == null ? '' : String(str);
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+}) ();
